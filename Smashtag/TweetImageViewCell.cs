de176b940441fb0bd2b73cd6fd8fbe44b23a98de
Antonio Feregrino <hs_mac@MacBook-Pro-de-Antonio.local>
2016-12-22 07:43:23 -0600

    More async

--- Smashtag/TweetImageViewCell.cs
+++ Smashtag/TweetImageViewCell.cs
@@ -13,15 +13,48 @@ namespace Smashtag
 		{
 		}
 
+
+		public Uri ImageURL
+		{
+			get;
+			set;
+		}
+
+		void FetchImage()
+		{
+			var url = ImageURL;
+			if (url != null)
+			{
+				new System.Threading.Thread(new System.Threading.ThreadStart(() =>
+				{
+					var contentsOfUrl = NSData.FromUrl(url);
+
+					InvokeOnMainThread(() =>
+					{
+						if (url == this.ImageURL)
+						{
+							var imageData = contentsOfUrl;
+							if (imageData != null)
+							{
+								tweetImageView.Image = new UIImage(imageData);
+								tweetImageView.ContentMode = UIViewContentMode.ScaleAspectFit;
+							}
+							else
+							{
+							}
+						}
+					});
+				})).Start();
+			}
+		}
+
 		public void SetImage(string url)
 		{
 
 			if (!String.IsNullOrEmpty(url))
 			{
-				var imageData = NSData.FromUrl(new Uri(url));
-				tweetImageView.Image = new UIImage(imageData);
-				tweetImageView.ContentMode = UIViewContentMode.ScaleAspectFit;
-				//tweetImageView.SizeToFit();
+				ImageURL = new Uri(url);
+				FetchImage();
 			}
 		}
 	}
