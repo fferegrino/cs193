f9eec9c6d38464d0a4f8f8b9e28cf953c507183b
Antonio Feregrino <antonio.feregrino@gmail.com>
2016-12-24 20:06:23 -0600

    DropIt bouncy thingies

---
+++ DropIt/DropItViewController.designer.cs
@@ -0,0 +1,24 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace DropIt
+{
+	[Register ("DropItViewController")]
+	partial class DropItViewController
+	{
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (gameView != null) {
+				gameView.Dispose ();
+				gameView = null;
+			}
+		}
+	}
+}
---
+++ DropIt/FallingObjectBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UIKit;
+
+namespace DropIt
+{
+	public class FallingObjectBehavior : UIDynamicBehavior
+	{
+
+		UIGravityBehavior gravity = new UIGravityBehavior();
+		UICollisionBehavior _collider;
+		UICollisionBehavior collider => _collider ?? (_collider = new UICollisionBehavior
+		{ TranslatesReferenceBoundsIntoBoundary = true });
+		UIDynamicItemBehavior _itemBehavior;
+		UIDynamicItemBehavior itemBehavior => _itemBehavior ?? (_itemBehavior = new UIDynamicItemBehavior
+		{ AllowsRotation = false,
+			Elasticity = 0.75f});
+
+		public FallingObjectBehavior()
+		{
+			AddChildBehavior(gravity);
+			AddChildBehavior(collider);
+			AddChildBehavior(itemBehavior);
+		}
+
+		public void AddItem(IUIDynamicItem item)
+		{
+			gravity.AddItem(item);
+			collider.AddItem(item);
+			itemBehavior.AddItem(item);
+		}
+
+		public void RemoveItem(IUIDynamicItem item)
+		{
+			gravity.RemoveItem(item);
+			collider.RemoveItem(item);
+			itemBehavior.RemoveItem(item);
+		}
+	}
+}
